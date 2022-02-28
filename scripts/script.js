24836277a25e6f309c622861a57beb107084905d
Franco Bianciotto <bianciottofranco@gmail.com>
2022-02-28 12:33:49 -0300

    add DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-class Producto{
-    constructor(id, nombre,precio, color, tamanio){
+class Producto {
+    constructor(id, nombre, precio, color, tamanio) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
@@ -7,30 +7,50 @@ class Producto{
         this.tamanio = tamanio;
     }
 }
-class Compra{
-    constructor(cantidad,nombre, precio){
-        this.nombre=nombre;
-        this.cantidad=cantidad;
-        this.precioFinal=precio*this.cantidad;
+class Compra {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.cantidad = 0;
+        this.precioFinal = precio;
     }
-    calcularTotal(propiedad){
-        return this.reduce((acumulador, elemento) => acumulador + (elemento[propiedad] || 0), 0);
-    }
+    
+    // calcularTotal(propiedad) {
+    //     return this.reduce((acumulador, elemento) => acumulador + (elemento[propiedad] || 0), 0);
+    // }
 }
-const producto1 = new Producto(1,"Alfombra Catania", 2600,"crudo","60x110cm")
-const producto2 = new Producto(2,"Alfombra Ciparicia", 16000,"rombo negro","120x200cm")
-const producto3 = new Producto(3,"Alfombra Redonda", 7400,"crudo","150cm")
-const producto4 = new Producto(4,"Alfombra Berlina", 5600,"lineas grises","90x110cm")
+const producto1 = new Producto(1, "Alfombra Catania", 2600, "crudo", "60x110cm")
+const producto2 = new Producto(2, "Alfombra Ciparicia", 16000, "rombo negro", "120x200cm")
+const producto3 = new Producto(3, "Alfombra Redonda", 7400, "crudo", "150cm")
+const producto4 = new Producto(4, "Alfombra Berlina", 5600, "lineas grises", "90x110cm")
 
-const productos=[producto1,producto2,producto3,producto4]
-let cantidad=menu();
-carrito=mostrarCarrito(cantidad,productos);
-console.log(carrito);
-const sumall = carrito.map(item => item.precioFinal).reduce((prev, curr) => prev + curr, 0);
-console.log(sumall);
+const productos = [producto1, producto2, producto3, producto4];
+
+let button1 = document.getElementById('btn1');
+let button2 = document.getElementById('btn2');
+let button3 = document.getElementById('btn3');
+let button4 = document.getElementById('btn4');
+
+const compras = new Array();
+
+button1.addEventListener('click', () => {
+    compras[0] = new Compra((productos[0].nombre), (productos[0].precio));
+    compras[0].cantidad++;
+});
+button2.onclick = () => (compras[1] = new Compra((productos[1].nombre), (productos[1].precio)), compras[1].cantidad++);
+button3.onclick = () =>(compras[2] = new Compra((productos[2].nombre), (productos[2].precio)), compras[2].cantidad++);
+button4.onclick = () =>(compras[3] = new Compra((productos[3].nombre), (productos[3].precio)), compras[3].cantidad++);
 
 
-function menu(){
+console.log(compras)
+const sumall = compras.map(item => item.precioFinal).reduce((prev, curr) => prev + curr, 0);
+console.log(sumall)
+//let cantidad=menu();
+//carrito=mostrarCarrito(cantidad,productos);
+//console.log(carrito);
+//const sumall = carrito.map(item => item.precioFinal).reduce((prev, curr) => prev + curr, 0);
+//console.log(sumall);
+
+/*function menu(){
     const seleccion= new Array(productos.length);
     reiniciarCarrito(seleccion);
     var termina = false;
@@ -69,3 +89,4 @@ function reiniciarCarrito(carrito){
         carrito[i]=0;
     }
 }
+*/
